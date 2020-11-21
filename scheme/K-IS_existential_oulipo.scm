75deb17bd25b8c93a997ac25031c3471130b7f1e
Charles Horn <charles.horn@gmail.com>
2020-11-21 21:55:24 +1300

    Golf the nano entry's Scheme code to 256 bytes

--- scheme/K-IS_existential_oulipo.scm
+++ scheme/K-IS_existential_oulipo.scm
@@ -1,11 +1,4 @@
-(load "LazyK/lazier.scm")
-(load "LazyK/prelude.scm")
-(load "LazyK/prelude-numbers.scm")
-
-(lazy-def 'K. '(o (cons 75) (o (cons 46) (o (cons 32) (o (cons 105) (cons 115))))))
-(lazy-def 'IS '(o (cons 44) (o (cons 32) (o (cons 105) (cons 115)))))
-
-(lazy-def '(main input)
-   '(o (K.) (o ((* 78 (1+ (* 64 10))) IS) (cons 46)) end-of-output))
-
-(print-as-unlambda (laze 'main))
+(load "LazyK/lazier")
+(load "LazyK/prelude")
+(load "LazyK/prelude-numbers")
+(print-as-unlambda(laze '(K(o(o(cons 75)(o(cons 46)(o(cons 32)(o(cons 105)(cons 115)))))(o((* 78(1+(* 64 10)))(o(cons 44)(o(cons 32)(o(cons 105)(cons 115)))))(cons 46))(K 256)))))
