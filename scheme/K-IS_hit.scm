ca8a5e854ccccc4ea985464cb9f95c2d648de9b2
Charles Horn <charles.horn@gmail.com>
2020-11-22 16:17:23 +1300

    add male name determinative, DIŠ, to protagonist

--- scheme/K-IS_hit.scm
+++ scheme/K-IS_hit.scm
@@ -9,7 +9,7 @@
 (lazy-def '(CUN2 a b) '(o (cons (* 10 24)) (o (cons (* 2 73)) (o (cons a) (cons (+ a b))))))
 
 ;(lazy-def 'QA '(CUN (1+ (* 2 69)) (* 7 23)))
-(lazy-def 'QA '(CUN2 (1+ (* 2 69)) 22))
+(lazy-def 'QA '(o (CUN2 (* 3 43) 56) (CUN2 (1+ (* 2 69)) 22)))
 (lazy-def 'ESZI '(o (cons 32) (o (CUN (* 10 13) (* 2 69)) (o (CUN (* 10 14) (* 3 47)) (CUN (* 3 47) (1+ (* 6 27)))))))
 (lazy-def 'ISKUNANZ '(o (cons 32) (o (CUN2 (* 7 19) 17) (o (CUN2 (* 2 67) 36) (o (CUN2 (* 4 34) 54) (o (CUN2 (* 8 16) 45) (CUN2 (* 3 47) 16)))))))
 
